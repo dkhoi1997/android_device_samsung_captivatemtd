0d83e93e2ad81d5601fa6cac7a57f4400bdf688b
dkhoi1997 <dkhoi1997@gmail.com>
2014-05-02 21:17:16 +0700

    captivatemtd: Kit Kat bring-up

--- omni_captivatemtd.mk
+++ omni_captivatemtd.mk
@@ -1,13 +1,28 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/captivatemtd/full_captivatemtd.mk)
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Release name
-PRODUCT_RELEASE_NAME := Captivate
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit device configuration
+$(call inherit-product, device/samsung/captivatemtd/full_captivatemtd.mk)
+
+# Device identifier
+PRODUCT_RELEASE_NAME := Captivate
 PRODUCT_DEVICE := captivatemtd
 PRODUCT_NAME := omni_captivatemtd
 PRODUCT_BRAND := samsung
